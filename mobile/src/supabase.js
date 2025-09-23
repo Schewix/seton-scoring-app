1bfa15c942895aa9d280720a848996999c5aeff1
Ondrej Sevcik <sevcik.ondrej11@gmail.com>
2025-09-23 10:31:42 +0200

    Remove mobile

--- mobile/src/supabase.js
+++
@@ -1,9 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-import Constants from 'expo-constants';
-
-const extra = Constants.expoConfig?.extra || {};
-export const supabase = createClient(
-  extra.EXPO_PUBLIC_SUPABASE_URL,
-  extra.EXPO_PUBLIC_SUPABASE_ANON_KEY,
-  { auth: { persistSession: true, autoRefreshToken: true } }
-);
